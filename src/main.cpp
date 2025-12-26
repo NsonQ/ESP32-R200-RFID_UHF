77305061d5cacfb4aff7e1b3bb0d093e6d6b2c70
NsonQ <ansonkiu0212@gmail.com>
2025-12-26 17:59:23 +0800

    read lock state for inventory scanning and cart

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,18 @@
 
 #define RX_PIN 16
 #define TX_PIN 17
+#define LOCK_PIN 21
+#define LOCK_FEEDBACK_PIN 22
 
 // MQTT Topics
 const char *INVENTORY = "Fridge01/Inventory";
 const char *COMMAND = "Fridge01/Command";
 const char *CART = "Fridge01/Cart";
+
+// Lock
+// High means unlocked, Low means locked
+int lastLockState = 0;
+String diff = "[]";
 
 R200 reader(Serial2, RX_PIN, TX_PIN);
 WiFiClient espClient;
@@ -29,6 +36,9 @@ void setup()
   reader.setTxPower(15);
   Serial.println("R200 Initialized");
   setup_wifi();
+  pinMode(LOCK_PIN, OUTPUT);
+  pinMode(LOCK_FEEDBACK_PIN, INPUT_PULLUP);
+  lastLockState = digitalRead(LOCK_FEEDBACK_PIN);
   Serial.println("WiFi Connected");
   client.setServer(mqtt_server, mqtt_port);
   Serial.println("Press 1 for scan, Press 2 for scan and diff");
@@ -36,32 +46,50 @@ void setup()
 
 void loop()
 {
+  int currentLockState = digitalRead(LOCK_FEEDBACK_PIN);
+
+  delay(100); // Small debounce delay
+
   if (!client.connected())
   {
     reconnect();
   }
   client.loop();
 
+  // Scan the initial inventory when the door is unlocked
+  if (currentLockState == HIGH && lastLockState == LOW)
+  {
+    previousInventory = reader.scan();
+    Serial.println("Initial Inventory: " + previousInventory);
+    lastLockState = currentLockState;
+  }
+  // Repeatedly scan when the door is opening
+  else if (currentLockState == HIGH && lastLockState == HIGH)
+  {
+    currentInventory = reader.scan();
+    Serial.println("Current Inventory: " + currentInventory);
+    delay(1000);
+  }
+  // Publish the updated inventory and cart when the door is closed
+  else if (currentLockState == LOW && lastLockState == HIGH)
+  {
+    diff = reader.getJsonDifference(previousInventory, currentInventory);
+    Serial.println("Cart: " + diff);
+    client.publish(CART, diff.c_str());
+    client.publish(INVENTORY, currentInventory.c_str());
+    lastLockState = currentLockState;
+  }
+
   if (Serial.available())
   {
     char cmd = Serial.read();
 
-    // Scan the initial inventory when the door is unlocked
-    if (cmd == '1')
+    if (cmd == 'o')
     {
-      Serial.println("Scanning...");
-      previousInventory = reader.scan();
-      Serial.println("Result: " + previousInventory);
-    }
-    // Repeatedly scan
-    // Publish the updated inventory and cart when the door is closed
-    else if (cmd == '2')
-    {
-      Serial.println("Checking out...");
-      currentInventory = reader.scan();
-      String diff = reader.getJsonDifference(previousInventory, currentInventory);
-      client.publish(CART, diff.c_str());
-      Serial.println(diff);
+      Serial.println("Door Unlocked");
+      digitalWrite(LOCK_PIN, HIGH);
+      delay(300);
+      digitalWrite(LOCK_PIN, LOW);
     }
   }
 }
@@ -72,7 +100,7 @@ void setup_wifi()
 {
   WiFi.mode(WIFI_STA);
   // WiFi.setTxPower(WIFI_POWER_8_5dBm);
-  //  Serial.print("Set Wifi to 8.5 dBm");
+  // Serial.print("Set Wifi to 8.5 dBm");
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED)
   {
